481fae39f732475b167c7600ce481b86d758bf52
Mostafa Tavassoli <m.tavassoli.it@gmail.com>
2020-07-23 15:23:50 -0700

    Build: Server-Side and Complete the APIs

--- server/server.js
+++ server/server.js
@@ -1,24 +1,23 @@
 //import express
 const express = require("express");
-const dao = require("./task_dao");
+const Task_dao = require("./task_dao");
 const morgan = require("morgan"); // logging middleware
 
 //create application
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Set-up logging
 app.use(morgan("tiny"));
 
-// Process body content
-app.use(express.json());
+// Process body content - for seting up parsing incoming JSON DATA - we have accessible as an OBJECT
+app.use(express.json()); // Automatically parse incomming JSON to an OBJECT
 
 // REST API endpoints
 
 //GET /tasks
 app.get("/tasks", (req, res) => {
-  dao
-    .getTasks(req.query.filter)
+  Task_dao.getTasks(req.query.filter)
     .then((tasks) => {
       res.json(tasks);
     })
@@ -29,4 +28,64 @@ app.get("/tasks", (req, res) => {
     });
 });
 
-app.listen(port, () => console.log("Server ready"));
+//GET /tasks/<taskId>
+app.get("/tasks/:taskId", (req, res) => {
+  Task_dao.getTask(req.params.taskId)
+    .then((task) => {
+      if (!task) {
+        res.status(400).end();
+      } else {
+        res.json(task);
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        errors: [{ param: "Server", msg: err }],
+      });
+    });
+});
+
+app.post("/tasks", (req, res) => {
+  const task = req.body;
+  if (!task) {
+    res.status(400).end();
+  } else {
+    Task_dao.createTask(task)
+      .then((id) => res.status(201).json({ id: id })) // 201 CREATED The request has been fulfilled and has resulted in one or more new resources being created. // source is https://httpstatuses.com/201
+      .catch((err) =>
+        res.status(500).json({
+          errors: [{ param: "Server", msg: err }],
+        })
+      );
+  }
+});
+
+//DELETE /tasks/<taskId>
+app.delete("/tasks/:taskId", (req, res) => {
+  Task_dao.deleteTask(req.params.taskId)
+    .then((result) => res.status(204).end())
+    .catch((err) =>
+      res.status(500).json({
+        errors: [{ param: "Server", msg: err }],
+      })
+    );
+});
+
+//PUT /tasks/<taskId>
+app.put("/tasks/:taskId", (req, res) => {
+  if (!req.body.id) {
+    res.status(400).end();
+  } else {
+    const task = req.body;
+    console.log(task);
+    Task_dao.updateTask(req.params.taskId, task)
+      .then((result) => res.status(200).end())
+      .catch((err) =>
+        res.status(500).json({
+          errors: [{ param: "Server", msg: err }],
+        })
+      );
+  }
+});
+
+app.listen(port, () => console.log(`Server is up on PORT ${port}`));
--- server/task_dao.js
+++ server/task_dao.js
@@ -1,8 +1,26 @@
 "use strict";
 
-const Task = require("./task");
+const Task = require("./models/task");
 const db = require("./db");
 const moment = require("moment");
+
+/**
+ * Function to check if a date is today. Returns true if the date is today, false otherwise.
+ * @param {*} date a Moment js date to be checked
+ */
+const isToday = function (date) {
+  return date.isSame(moment(), "day");
+};
+
+/**
+ * Function to check if a date is in the next week. Returns true if the date is in the next week, false otherwise.
+ * @param {*} date a Moment js Date to be checked
+ */
+const isNextWeek = function (date) {
+  const nextWeek = moment().add(1, "weeks");
+  const tomorrow = moment().add(1, "days");
+  return date.isAfter(tomorrow) && date.isBefore(nextWeek);
+};
 
 /**
  * Function to create a Task object from a row of the tasks table
@@ -71,5 +89,91 @@ exports.getTasks = function (filter) {
         resolve(tasks);
       }
     });
+  });
+};
+
+exports.getTask = function (id) {
+  return new Promise((resolve, reject) => {
+    const sql = "SELECT * FROM tasks WHERE id = ?";
+    db.all(sql, [id], (err, rows) => {
+      if (err) reject(err);
+      else if (rows.length === 0) resolve(undefined);
+      else {
+        const task = createTask(rows[0]);
+        resolve(task);
+      }
+    });
+  });
+};
+
+/**
+ * Insert a task in the database and returns the id of the inserted task.
+ * To get the id, this.lastID is used. To use the "this", db.run uses "function (err)" instead of an arrow function.
+ */
+exports.createTask = function (task) {
+  return new Promise((resolve, reject) => {
+    const sql =
+      "INSERT INTO tasks(description, important, private, project, deadline, completed) VALUES(?,?,?,?,DATETIME(?),?)";
+    db.run(
+      sql,
+      [
+        task.description,
+        task.important,
+        task.privateTask,
+        task.project,
+        task.deadline,
+        task.completed,
+      ],
+      function (err) {
+        if (err) {
+          console.log(err);
+          reject(err);
+        } else {
+          console.log(this.lastID);
+          resolve(this.lastID);
+        }
+      }
+    );
+  });
+};
+
+/**
+ * Delete a task with a given id
+ */
+exports.deleteTask = function (id) {
+  return new Promise((resolve, reject) => {
+    const sql = "DELETE FROM tasks WHERE id = ?";
+    db.run(sql, [id], (err) => {
+      if (err) reject(err);
+      else resolve(null);
+    });
+  });
+};
+
+/**
+ * Update an existing task with a given id. newTask contains the new values of the task (e.g., to mark it as "completed")
+ */
+exports.updateTask = function (id, newTask) {
+  return new Promise((resolve, reject) => {
+    const sql =
+      "UPDATE tasks SET description = ?, important = ?, private = ?, project = ?, deadline = DATETIME(?), completed = ? WHERE id = ?";
+    db.run(
+      sql,
+      [
+        newTask.description,
+        newTask.important,
+        newTask.privateTask,
+        newTask.project,
+        newTask.deadline,
+        newTask.completed,
+        id,
+      ],
+      (err) => {
+        if (err) {
+          console.log(err);
+          reject(err);
+        } else resolve(null);
+      }
+    );
   });
 };
